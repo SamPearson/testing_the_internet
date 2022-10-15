6b4cb87ebeba87c080737bbae60d9e3ab9f64738
Sam Pearson <sam.ed.pearson@gmail.com>
2022-10-15 11:09:01 -0500

    Adding second test for failed login (bad credentials)

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,14 +1,20 @@
 from selenium.webdriver.common.by import By
 
 class LoginPage:
+    _login_form = {"by": By.ID, "value": "login"}
     _username_input = {"by": By.ID, "value": "username"}
     _password_input = {"by": By.ID, "value": "password"}
     _submit_button = {"by": By.CSS_SELECTOR, "value": "button"}
+
     _success_message = {"by": By.CSS_SELECTOR, "value": ".flash.success"}
+    _failure_message = {"by": By.CSS_SELECTOR, "value": ".flash.error"}
 
     def __init__(self, driver):
         self.driver = driver
         self.driver.get("http://the-internet.herokuapp.com/login")
+        assert self.driver.find_element(
+            self._login_form["by"], self._login_form["value"]).is_displayed()
+
 
     def with_(self, username, password):
         self.driver.find_element(self._username_input["by"],
@@ -21,4 +27,8 @@ class LoginPage:
     def success_message_present(self):
         return self.driver.find_element(
             self._success_message["by"], self._success_message["value"]).is_displayed()
+
+    def failure_message_present(self):
+        return self.driver.find_element(
+            self._failure_message["by"], self._failure_message["value"]).is_displayed()
 
--- tests/first_test.py
+++ tests/first_test.py
@@ -21,3 +21,7 @@ class TestLogin:
     def test_valid_credentials(self, login):
         login.with_("tomsmith", "SuperSecretPassword!")
         assert login.success_message_present()
+
+    def test_invalid_credentials(self, login):
+        login.with_("tomsmith", "bad password")
+        assert login.failure_message_present()
