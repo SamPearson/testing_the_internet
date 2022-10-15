7ffb8e468f6797525b7a2095a14a688cd05bbf11
Sam Pearson <sam.ed.pearson@gmail.com>
2022-10-15 14:04:18 -0500

    Abstracting basic selenium functions into a BasePage class

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
-class LoginPage:
+
+class LoginPage(BasePage):
     _login_form = {"by": By.ID, "value": "login"}
     _username_input = {"by": By.ID, "value": "username"}
     _password_input = {"by": By.ID, "value": "password"}
@@ -11,24 +13,16 @@ class LoginPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.driver.get("http://the-internet.herokuapp.com/login")
-        assert self.driver.find_element(
-            self._login_form["by"], self._login_form["value"]).is_displayed()
-
+        self._visit("http://the-internet.herokuapp.com/login")
+        assert self._is_displayed(self._login_form)
 
     def with_(self, username, password):
-        self.driver.find_element(self._username_input["by"],
-                                 self._username_input["value"]).send_keys(username),
-        self.driver.find_element(self._password_input["by"],
-                                 self._password_input["value"]).send_keys(password),
-        self.driver.find_element(self._submit_button["by"],
-                                 self._submit_button["value"]).click()
+        self._type(self._username_input, username)
+        self._type(self._password_input, password)
+        self._click(self._submit_button)
 
     def success_message_present(self):
-        return self.driver.find_element(
-            self._success_message["by"], self._success_message["value"]).is_displayed()
+        return self._is_displayed(self._success_message)
 
     def failure_message_present(self):
-        return self.driver.find_element(
-            self._failure_message["by"], self._failure_message["value"]).is_displayed()
-
+        return self._is_displayed(self._failure_message)
