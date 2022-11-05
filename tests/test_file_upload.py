657546942fcc56595c29e18378a25c370544af04
Sam Pearson <sam.ed.pearson@gmail.com>
2022-11-05 12:59:26 -0500

    Upload test

---
+++ tests/test_file_upload.py
@@ -0,0 +1,20 @@
+import pytest
+from pages import file_upload_page
+import os
+
+
+@pytest.mark.upload
+class TestFileUpload:
+    @pytest.fixture
+    def upload(self, driver):
+        return file_upload_page.UploadPage(driver)
+
+    @pytest.mark.smoke
+    def test_upload_file(self, upload):
+        #create file
+        filename = "upload_test_file.txt"
+        path = os.path.join(os.getcwd(), filename)
+        upload.file_(path)
+
+        assert upload.uploaded_filename() == filename
+
