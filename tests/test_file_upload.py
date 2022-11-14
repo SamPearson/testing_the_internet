31a862baa1b47954258b5aa059071fdecd5c6e54
Sam Pearson <sam.ed.pearson@gmail.com>
2022-11-14 17:01:41 -0600

    Creating directory for test assets

--- tests/test_file_upload.py
+++ tests/test_file_upload.py
@@ -13,7 +13,7 @@ class TestFileUpload:
     def test_upload_file(self, upload):
         #create file
         filename = "upload_test_file.txt"
-        path = os.path.join(os.getcwd(), filename)
+        path = os.path.join(os.getcwd(), "assets", filename)
         upload.file_(path)
 
         assert upload.uploaded_filename() == filename
